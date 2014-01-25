d38034cc33cda925b9c091efba5f276ca165dd4a
PearBit <Rashad--91@hotmail.com>
2014-01-25 11:27:12 +0200

    Player now has jump and move methods

--- fgjplatform/src/com/me/fgjplatform/GameScreen.java
+++ fgjplatform/src/com/me/fgjplatform/GameScreen.java
@@ -17,7 +17,7 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class GameScreen extends DefaultScreen implements InputProcessor  {
-	
+
 	private static OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Texture texture;
@@ -27,61 +27,59 @@ public class GameScreen extends DefaultScreen implements InputProcessor  {
 	private Body playerBody;
 	private CreatureObject player;
 	static final float BOX_STEP=1/60f;  
-    static final int BOX_VELOCITY_ITERATIONS=6;  
-    static final int BOX_POSITION_ITERATIONS=2; 
-    
-    private MapCreator mapCreator;
+	static final int BOX_VELOCITY_ITERATIONS=6;  
+	static final int BOX_POSITION_ITERATIONS=2; 
+
+	private MapCreator mapCreator;
 
 	public GameScreen(Game game) {
 		super(game);
-		
-		world = new World(new Vector2(0, -10), true); 
+
+		Gdx.input.setInputProcessor(this);
+
+		world = new World(new Vector2(0, -10), true);
 		mapCreator = new MapCreator(world);
 		camera = new OrthographicCamera();  
-        camera.viewportHeight = Global.HEIGHT;  
-        camera.viewportWidth = Global.WIDTH;  
-        camera.position.set(camera.viewportWidth * .5f, camera.viewportHeight * .5f, 0f);  
-        camera.update();  
-		
+		camera.viewportHeight = Global.HEIGHT;  
+		camera.viewportWidth = Global.WIDTH;  
+		camera.position.set(camera.viewportWidth * .5f, camera.viewportHeight * .5f, 0f);  
+		camera.update();  
+
 		player = mapCreator.getPhysicalPlayer();
 		playerBody = player.GetBody();
-		
+
 		batch = new SpriteBatch();
-		
+
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
+
 		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
+
 		sprite = new Sprite(region);
 		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
 		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
 		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
 	}
-	
+
 	@Override
 	public void dispose() {
 		batch.dispose();
 		texture.dispose();
 	}
-	
+
 	@Override
 	public void render(float delta) {		
-		Vector2 position = playerBody.getPosition();
-		if(Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.A)) {
-			playerBody.applyLinearImpulse(-10f, 0, position.x, position.y, true);
-		}
-		
+
 		//Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
+
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		
+
 		for (BaseObject o :mapCreator.getRectDynamicObjects()) {
 			o.draw(batch);
 		}
-		
+
 		batch.end();
 		debugRenderer.render(world, camera.combined);
 		world.step(1/60f, 6, 2);
@@ -89,7 +87,19 @@ public class GameScreen extends DefaultScreen implements InputProcessor  {
 
 	@Override
 	public boolean keyDown(int keycode) {
-		// TODO Auto-generated method stub
+		if(keycode == Keys.SPACE){
+			player.jump();
+			System.out.println("jump");
+		}
+
+		if(keycode == Keys.A){
+			player.move(-1000f);
+		}
+		
+		if(keycode == Keys.D){
+			player.move(1000f);
+		}
+
 		return false;
 	}
 
