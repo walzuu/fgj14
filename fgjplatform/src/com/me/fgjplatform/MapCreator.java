b1f1a79373918008e6113a5ab042c8ccd11650eb
sheepbeo <lequoccuong91@yahoo.com>
2014-01-25 16:23:53 +0200

    test zoom map done

--- fgjplatform/src/com/me/fgjplatform/MapCreator.java
+++ fgjplatform/src/com/me/fgjplatform/MapCreator.java
@@ -29,21 +29,29 @@ public class MapCreator {
 	private void initDynamicObjects() {
 		// player characters
 		creatureObjects.add(new AlienObject(100f, 10f + 50f, 50f, 100f,this.world));
-		creatureObjects.add(new RobotObject(200f, 10f + 25f, 20f, 50f,this.world));
+		creatureObjects.add(new RobotObject(400f, 10f + 25f, 20f, 50f,this.world));
 		
-		rectDynamicObjects.add(new BaseRectDynamicObject(600f, 160f, 100f, 100f,this.world));
-		rectDynamicObjects.add(new BaseRectDynamicObject(600f, 60f, 100f, 100f,this.world));
+		rectDynamicObjects.add(new BaseRectDynamicObject(900f, 160f, 100f, 100f,this.world));
+		rectDynamicObjects.add(new BaseRectDynamicObject(900f, 60f, 100f, 100f,this.world));
 	}
 	
 	private  void initStaticObjects() {
-		staticObjects.add(new Ground(0f + 1000f, -40f - 50f, 2000f, 100f, this.world));
+		staticObjects.add(new Ground(-1000f + 2000f, -190f + 50f, 4000f, 100f, this.world));
+		staticObjects.add(new Ground(-1000f + 250f, -90f + 500f, 500f, 1000f, this.world));
 		
-		staticObjects.add(new Water(200f + 50f, 10f - 50f, 100f, 100f, this.world));
+		staticObjects.add(new Ground(-500f + 800f, -90f + 50f, 1600f, 100f, this.world));
+		staticObjects.add(new Water(1100f + 50f, -90f + 50f, 100f, 100f, this.world));
+		staticObjects.add(new Ground(1200f + 50f, -90f + 50f, 100f, 100f, this.world));
+		staticObjects.add(new Water(1300f + 50f, -90f + 50f, 100f, 100f, this.world));
+		staticObjects.add(new Ground(1400f + 250f, -90f + 50f, 500f, 100f, this.world));
+		staticObjects.add(new Ground(1900f + 250f, -90f + 500f, 500f, 1000f, this.world));
 		
-		staticObjects.add(new Ground(0f + 100f, 10f - 50f, 200f, 100f, this.world));
-		staticObjects.add(new Ground(300f + 200f, 10f - 50f, 400f, 100f, this.world));
-		staticObjects.add(new Tree(400f + 20f, 10f + 50f, 20f, 100f, this.world));
-		staticObjects.add(new Door(800f + 25f, 10f + 50f, 50f, 100f, this.world));
+		//staticObjects.add(new Ground(-500f + 350f, -90f + 50f, 700f, 100f, this.world));
+		//staticObjects.add(new Ground(300f + 200f, -90 + 50f, 400f, 100f, this.world));
+		
+		staticObjects.add(new Tree(300f + 20f, 10f + 50f, 20f, 100f, this.world));
+		staticObjects.add(new Tree(700f + 20f, 10f + 50f, 20f, 100f, this.world));
+		staticObjects.add(new Door(1500f + 25f, 10f + 50f, 50f, 100f, this.world));
 	}
 	
 	public  CreatureObject getPhysicalPlayer(int index) {
